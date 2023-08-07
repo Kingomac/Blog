28988843d006c058965604b98fbe386e87b82e84
Kingomac <31375644+Kingomac@users.noreply.github.com>
2023-08-07 08:44:40 +0000

    Sitemaps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  site: 'https://kingomac.github.io/',
+  base: '/Blog',
+  integrations: [mdx(), sitemap()]
 });
